e443ed65c59fcecebfd5194ad64127c39cde1c50
Admin <Admin@DESKTOP-1I2K01G>
2024-01-19 13:05:12 +0530

    first commit

---
+++ AishuProject/src/test/java/com/maven/aishu/Login.java
@@ -0,0 +1,12 @@
+package com.maven.aishu;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Login {
+
+	@Test
+	public void username() {
+		Reporter.log("username",true);
+	}
+}
